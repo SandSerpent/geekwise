cd1d205ad03204fe8e4dafb3a932a57712768dcc
student-03 <student-03>
2017-01-30 15:50:08 -0800

    jan30-variables

---
+++ jan30-jsvariables/js/script.js
@@ -0,0 +1,20 @@
+//alert('this an alert');
+
+var myNumberVar = 7; // Number Object
+var myFloatVar = 5.2; //Float Object
+var myStringVar = "Alfredo Colunga"; // String Object
+var myBooLeanVar = true; // Boolean Object
+var myArrayVar = ['one', 'stingray', 'banana', 8, false]; // Array Object
+var myObjectVar = {
+  make: 'Jeep',
+  model: 'wrangler',
+  color: 'green',
+  year: 2019
+}; // Object Object 
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooLeanVar);
+console.log(typeof myArrayVar);
+console.log(myObjectVar.model);
