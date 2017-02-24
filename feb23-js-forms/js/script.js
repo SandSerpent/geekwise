f5d02f2dc55330630c87fdd1a73a2102f9a32996
student-03 <student-03>
2017-02-24 15:32:56 -0800

    feb24-js-forms

--- feb23-js-forms/js/script.js
+++ feb23-js-forms/js/script.js
@@ -4,9 +4,26 @@
 // var d = document.querySelector();
 // var e = document.querySelectorAll();
 
-var form = document.querySelector('form');
-var subBtn = document.querySelector('input[type="submit"]');
+var form = document.querySelector('#contactForm');
+    subBtn = document.querySelector('#contactForm input[type="submit"]'),
+    loginForm = document.querySelector('#loginForm'),
+    loginBtn = document.querySelector('#loginForm input[type="submit"]');
+    custom =  document.querySelector('.custom');
+
+var loginArr = [];
 var userArr = [];
+
+var username = '';
+if(localStorage.getItem('username')){
+  username = localStorage.getItem('username');
+  alert(username + 'please enter your password to continue');
+  password();
+}else{
+  alert('please log in boi');
+  login();
+}
+console.log(username);
+
 
 subBtn.addEventListener('click', function(evt){
     evt.preventDefault();
@@ -20,3 +37,37 @@ subBtn.addEventListener('click', function(evt){
     }
     console.log(userArr);
 });
+
+loginBtn.addEventListener('click', function(evt){
+    evt.preventDefault();
+
+
+    for(var i = 0; i < loginForm.elements.length - 1; i++){
+      if(loginForm.elements[i].type === 'text'){
+        localStorage.setItem('username', loginForm.elements[i].value);
+      }
+      if(loginForm.elements[i].type === 'password'){
+        sessionStorage.setItem('password', MD5(loginForm.elements[i].value));
+      }
+      custom.textContent = 'Hello ' +localStorage.getItem('username')+ '.';
+      for(var i = 0; i < contactForm.length; i++){
+        contactForm.elements[i].disabled = false;
+      }
+    }
+});
+function password(){
+  loginBtn.addEventListener('click', function(evt){
+    evt.preventDefault();
+    if(MD5(loginForm.elements[1].value) === sessionStorage.getItem('password')){
+      alert('passwords match. good job, continue');
+    }else{
+      alert('your password was wron. you suck.');
+    }
+  })
+}
+/*
+  .setItem('key', 'value');
+  .getItem('key');
+  .removeItem('key');
+  .clear();
+*/
