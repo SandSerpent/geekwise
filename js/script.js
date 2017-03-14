28c705af90058476dcdb0dab55a0ba5cb02b0878
student-03 <student-03>
2017-03-14 15:57:48 -0700

    portfolio update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
-var snOpen = document.getElementById('open'),
-    snClose = document.getElementById('close'),
-    sn = document.getElementById('sn'),
-    main = document.getElementById('main');
+var inpName = document.querySelector('input:nth-of-type(1)'),
+    inpMail = document.querySelector('input:nth-of-type(2)'),
+    inpSub = document.querySelector('input:nth-of-type(3)'),
+    bio = document.querySelector('main section:nth-of-type(2) span:nth-of-type(3)');
 
-snOpen.addEventListener("click", openNav);
-snClose.addEventListener("click", closeNav);
+inpSub.addEventListener('click', function(evt){
+  evt.preventDefault();
+  var fullName = inpName.value.trim(),
+      email = inpMail.value,
+      subBtn = inpSub.value;
 
-function openNav(){
-  sn.classList.add('open');
-  snOpen.style.display = "none";
-  snClose.style.display = "block";
-  main.classList.add('small');
-}
+      alert('Hello ' + fullName + ", welcome to my webpage");
+      var contact = prompt('Is ' + email + ' your correct E-mail? If so, type "yes", if not type "no"').toLowerCase();
+      console.log(contact);
+      if(contact === "yes"){
+        alert("Alright! I'll contact you as soon as I can.");
+      }else{
+        alert("If you would like to contact me, fill in the correct information");
+      }
+      bio.textContent = " " + fullName;
 
-function closeNav(){
-  sn.classList.remove('open');
-  snOpen.style.display = "block";
-  snClose.style.display = "none";
-  main.classList.remove('small');
-}
+  var fullName = '',
+      email = '',
+      subBtn = '',
+      contact = '';
+      console.log(fullName);
+      console.log(email);
+      console.log(subBtn);
+});
+
+var footer = document.querySelector('footer'),
+    infinity = document.querySelector('footer span'),
+    astro = document.querySelector('footer i');
+    console.log(astro);
+
+footer.addEventListener('mouseover', function(){
+  astro.classList.add('floating');
+  infinity.style.textContent = 'to infinity';
+})
+footer.addEventListener('mouseout', function(){
+  astro.classList.remove('floating');
+})
